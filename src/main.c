dc4e7130002c4d720b0b3be0430c64d8c97465ee
joaosimsic <jpsimsic@hotmail.com>
2026-04-23 22:18:34 -0300

    feat: game loop

--- src/main.c
+++ src/main.c
@@ -1,17 +1,56 @@
 #include "common.h"
+#include "player.h"
+#include "render.h"
 #include "wfc.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <termios.h>
+#include <time.h>
+#include <unistd.h>
+
+static struct termios orig_term;
+
+static void disable_raw(void) {
+  tcsetattr(STDIN_FILENO, TCSAFLUSH, &orig_term);
+  printf("\033[?25h");
+}
+
+static void enable_raw(void) {
+  struct termios raw;
+
+  tcgetattr(STDIN_FILENO, &orig_term);
+  raw = orig_term;
+  raw.c_lflag &= ~(ECHO | ICANON);
+  raw.c_cc[VMIN] = 1;
+  tcsetattr(STDIN_FILENO, TCSAFLUSH, &raw);
+  printf("\033[?25l");
+}
 
 int main() {
-  printf("initilizing superposition...\n");
+  srand(time(NULL));
+
   init_map();
+  while (wfc_step())
+    ;
 
-  int entropy_count = count_possibilities(map[0][0].possible);
-  printf("System check: Cell [0][0] has %d possible states.\n", entropy_count);
+  init_player();
+  enable_raw();
+  atexit(disable_raw);
 
-  if (entropy_count == 4) {
-    printf("Engine ready. Reality is unwritten.\n");
-  } else {
-    printf("Core system failure.\n");
+  while (1) {
+    render_frame();
+    char c = getchar();
+
+    if (c == 'q')
+      break;
+    if (c == 'w')
+      move_player(-1, 0);
+    if (c == 's')
+      move_player(1, 0);
+    if (c == 'a')
+      move_player(0, -1);
+    if (c == 'd')
+      move_player(0, 1);
   }
 
   return 0;
